76eee89844d5940ea65bd5ba39c58c5cb8f828bb
Dominik Čustović <domcus44@gmail.com>
2020-05-12 23:08:24 +0200

    added controllers

---
+++ controllers/userController.js
@@ -0,0 +1,12 @@
+exports.login = function() {
+
+};
+exports.logout = function() {
+
+};
+exports.register = function(req, res) {
+    res.send("Thanks for registering")
+};
+exports.home = function(req, res) {
+    res.render('homepage')
+};
