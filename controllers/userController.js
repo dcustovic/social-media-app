8ee097193e5232556c8b2311ab1f160a23c4f809
Dominik Čustović <domcus44@gmail.com>
2020-05-13 20:18:59 +0200

    adding validation to registration fields

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,7 +11,11 @@ exports.logout = function() {
 exports.register = function(req, res) {
     let user = new User(req.body);
     user.register();
-    res.send("Thanks for registering");
+    if (user.errors.length) {
+        res.send(user.errors)
+    } else {
+        res.send("There are no errors.")
+    }
 };
 
 exports.home = function(req, res) {
--- models/User.js
+++ models/User.js
@@ -1,13 +1,55 @@
 // constructor function, reusable blueprint
+const validator = require('validator');
+
+
 let User = function(data) {
     this.data = data;
+    this.errors = []
 }
 
 User.prototype.validate = function() {
 
+    // Username validation
+    if (this.data.username == "") {
+        this.errors.push("You must provide a username.")
+    }
+
+    if (this.data.username != "" && !validator.isAlphanumeric(this.data.username)) {
+        this.errors.push("Username can only contain letters and numbers.")
+    }
+
+    if (this.data.username > 0 && this.data.username < 2) {
+        this.errors.push("Username must be at least 2 characters long.")
+    }
+
+    if (this.data.username > 30) {
+        this.errors.push("Username cannot exceed 30 characters.")
+    }
+
+    // Email validation
+    if (!validator.isEmail(this.data.email)) {
+        this.errors.push("You must provide a valid email address.")
+    }
+
+    // Password validation
+    if (this.data.password == "") {
+        this.errors.push("You must provide a password.")
+    }
+
+    if (this.data.password > 0 && this.data.password < 6) {
+        this.errors.push("Password must be at least 6 characters long.")
+    }
+
+    if (this.data.password > 32) {
+        this.errors.push("Password cannot exceed 32 characters.")
+    }
 }
 
+
+
 User.prototype.register = function() {
+    // Validate user data
+    this.validate()
 
 }
 
