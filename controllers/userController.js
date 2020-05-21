a224f771026797cd9213201ce8843442bde7f812
Dominik Čustović <domcus44@gmail.com>
2020-05-21 13:10:12 +0200

    restriction on email/username duplicate

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,7 @@ const User = require('../models/User');
 exports.login = function(req, res) {
 	let user = new User(req.body);
 	user.login().then(function(resolve){
-		req.session.user = {email: user.data.email}
+		req.session.user = {username: user.data.username}
 		req.session.save(function() {
 			res.redirect('/')
 		}) 
@@ -26,23 +26,25 @@ exports.logout = function(req, res) {
 
 exports.register = function(req, res) {
 	let user = new User(req.body);
-	user.register();
-
-	if (user.errors.length) {
-			user.errors.forEach(function(error) {
-				req.flash('regErrors', error)
-			})
-			req.session.save(function() {
-				res.redirect('/')
-			})
-	} else {
-			res.render('register')
-	}
+	
+	user.register().then(() => {
+		req.session.user = {username: user.data.username}
+		req.session.save(function() {
+			res.redirect('/')
+		})
+	}).catch((regErrors) => {
+		regErrors.forEach(function(error) {
+			req.flash('regErrors', error)
+		})
+		req.session.save(function() {
+			res.redirect('/')
+		})
+	})
 };
 
 exports.home = function(req, res) {
 	if (req.session.user) {
-		res.render('home-dashboard', {name: req.session.user.email})
+		res.render('home-dashboard', {username: req.session.user.username})
 	} else {
 		res.render('homepage', {logErrors: req.flash('logErrors'), regErrors: req.flash('regErrors')})
 	}
--- models/User.js
+++ models/User.js
@@ -9,47 +9,60 @@ let User = function(data) {
 	this.errors = []
 }
 
-
 User.prototype.validate = function() {
+	return new Promise(async (resolve, reject) => {
 
-	// Username validation
-	if (this.data.username == "") {
-			this.errors.push("You must provide a username.")
-	}
+		// Username validation
+		if (this.data.username == "") {
+				this.errors.push("You must provide a username.")
+		}
+	
+		if (this.data.username != "" && !validator.isAlphanumeric(this.data.username)) {
+				this.errors.push("Username can only contain letters and numbers.")
+		}
+	
+		if (this.data.username.length > 0 && this.data.username.length < 2) {
+				this.errors.push("Username must be at least 2 characters long.")
+		}
+	
+		if (this.data.username.length > 30) {
+				this.errors.push("Username cannot exceed 30 characters.")
+		}
+	
+		// Email validation
+		if (!validator.isEmail(this.data.email)) {
+				this.errors.push("You must provide a valid email address.")
+		}
+	
+		// Password validation
+		if (this.data.password == "") {
+				this.errors.push("You must provide a password.")
+		}
+	
+		if (this.data.password.length > 0 && this.data.password.length < 6) {
+				this.errors.push("Password must be at least 6 characters long.")
+		}
+	
+		if (this.data.password.length > 50) {
+				this.errors.push("Password cannot exceed 50 characters.")
+		}
+	
+		if (this.data.password !== this.data.passwordConfirm) {
+			this.errors.push("Passwords do not match.")
+		}
 
-	if (this.data.username != "" && !validator.isAlphanumeric(this.data.username)) {
-			this.errors.push("Username can only contain letters and numbers.")
-	}
+		// check to see if email or username already exists
+		if (this.data.username.length > 2 && this.data.username.length < 31 && validator.isAlphanumeric(this.data.username)) {
+			let usernameExists = await usersCollection.findOne({username: this.data.username})
+			if (usernameExists) {this.errors.push("The username is already taken.")}
+		}
 
-	if (this.data.username.length > 0 && this.data.username.length < 2) {
-			this.errors.push("Username must be at least 2 characters long.")
-	}
-
-	if (this.data.username.length > 30) {
-			this.errors.push("Username cannot exceed 30 characters.")
-	}
-
-	// Email validation
-	if (!validator.isEmail(this.data.email)) {
-			this.errors.push("You must provide a valid email address.")
-	}
-
-	// Password validation
-	if (this.data.password == "") {
-			this.errors.push("You must provide a password.")
-	}
-
-	if (this.data.password.length > 0 && this.data.password.length < 6) {
-			this.errors.push("Password must be at least 6 characters long.")
-	}
-
-	if (this.data.password.length > 50) {
-			this.errors.push("Password cannot exceed 50 characters.")
-	}
-
-	if (this.data.password !== this.data.passwordConfirm) {
-		this.errors.push("Passwords do not match.")
-	}
+		if (validator.isEmail(this.data.email)) {
+			let emailExists = await usersCollection.findOne({email: this.data.email})
+			if (emailExists) {this.errors.push("The email is already taken.")}
+		}
+		resolve();
+	});
 }
 
 User.prototype.cleanUp = function() {
@@ -84,11 +97,11 @@ User.prototype.login = function() {
 	return new Promise((resolve, reject) => {
 		this.cleanUp();
 
-		usersCollection.findOne({email: this.data.email}).then((emailFound) => {
-			if (emailFound && bcrypt.compareSync(this.data.password, emailFound.password)) {
+		usersCollection.findOne({username: this.data.username}).then((usernameFound) => {
+			if (usernameFound && bcrypt.compareSync(this.data.password, usernameFound.password)) {
 				resolve("You are logged in.")
 			} else {
-				reject("The email and password you entered did not match. Please double-check and try again.")
+				reject("The username and password you entered did not match. Please double-check and try again.")
 			}
 		}).catch(function() {
 			reject("Please try again later.")
@@ -98,18 +111,23 @@ User.prototype.login = function() {
 }
 
 User.prototype.register = function() {
-	// Validate user data
-	this.cleanUp();
-	this.validate();
-
-	// If there are no validation errors then save to database
-	if (!this.errors.length) {
-			// hash user password
-			let salt = bcrypt.genSaltSync(10);
-			this.data.password = bcrypt.hashSync(this.data.password, salt);
-
-			usersCollection.insertOne(this.data);
-	};
+	return new Promise(async (resolve, reject) => {
+		// Validate user data
+		this.cleanUp();
+		await this.validate();
+	
+		// If there are no validation errors then save to database
+		if (!this.errors.length) {
+				// hash user password
+				let salt = bcrypt.genSaltSync(10);
+				this.data.password = bcrypt.hashSync(this.data.password, salt);
+	
+				await usersCollection.insertOne(this.data);
+				resolve();
+		} else {
+			reject(this.errors);
+		}
+	})
 }
 
 
