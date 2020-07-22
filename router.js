4e60d029198444044b96bc5f423d5ed73eb81a5e
dcustovic <domcus44@gmail.com>
2020-07-22 09:48:38 +0200

    config Following

--- router.js
+++ router.js
@@ -13,6 +13,7 @@ router.post('/logout', userController.logout);
 // profile routes
 router.get('/profile/:username', userController.ifUserExists, userController.sharedProfileData, userController.profilePostsScreen);
 router.get('/profile/:username/followers', userController.ifUserExists, userController.sharedProfileData, userController.profileFollowersScreen);
+router.get('/profile/:username/following', userController.ifUserExists, userController.sharedProfileData, userController.profileFollowingScreen);
 
 // post routes
 router.get('/create-post', userController.mustBeLoggedIn, postController.viewCreateScreen);
